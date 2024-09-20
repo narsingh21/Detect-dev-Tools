8c558cf7700b455f30acf077845f2787b275c221
au-383985 <narsingh.mali@aubank.in>
2024-09-20 14:23:43 +0530

    remove buid error

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-import { DragEvent, useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import "./App.css";
-import DragbleElement from "./component/dragble.component";
 
-const leftBox = [
-  {
-    id: "1",
-    content: "box1",
-  },
-  {
-    id: "2",
-    content: "box2",
-  },
-];
 function App() {
-  const [rightBox, setRightBox] = useState<any>([]);
-
   const rootRef = useRef<HTMLDivElement>(null);
-
-  const onDrag = (e: DragEvent<HTMLDivElement>) => {
-    e.dataTransfer.setData("textplain", "");
-  };
-
-  const onDrop = (e: DragEvent<HTMLDivElement>, item: any) => {
-    //   if (e.clientX != 0 && e.clientY) {
-    //   }
-    // };
-
-    setRightBox((prev: any) => [
-      ...prev,
-      { element: item, position: { x: e.clientX, y: e.clientY } },
-    ]);
-    console.log(e.clientX, e.clientY);
-    console.log(item);
-  };
 
   const devtoolsOpen = () => {
     const threshold = 160;
@@ -68,66 +38,17 @@ function App() {
     }
   }, 1000);
 
-  // if (rootRef.current) {
-  //   const checkFullScreen = rootRef.current.onfullscreenerror;
-  //   const checkFullScreens = rootRef.current.onfullscreenchange;
-  //   console.log(checkFullScreen, checkFullScreens);
-
-  // }
-  //   useEffect(() => {
-
-  //   const full = document.fullscreenE;
-
-  //   console.log(full);
-  // }, [rightBox]);
-
-  // console.log('Is DevTools open:', window.devtoo;
-  // const checkFull = document.fullscreenElement;
-
-  useEffect(() => {
-    // window.addEventListener("devtoolschange", () => {
-    //   console.log("dev-tool change");
-    // });
-  }, []);
-
   return (
     <div
       ref={rootRef}
-      className="flex justify-center items-center relative h-screen"
+      className="flex justify-center flex-col items-center relative h-screen"
     >
-      {/* <div className="border-2 flex flex-col bg-red h-screen w-1/2">
-        {leftBox.map((el) => (
-          <div
-            draggable
-            onDragStart={(event) => onDrag(event)}
-            onDragEnd={(event) => onDrop(event, el)}
-            className="p-4  w-20  border bg-slate-500 text-gray-50"
-            key={el.id}
-          >
-            {el.content}
-          </div>
-        ))}
-      </div>
-      <div className="border-2   h-screen w-1/2">
-        {rightBox.map((el: any, index: number) => (
-          <DragbleElement key={index} position={el.position}>
-            {" "}
-            <div
-              draggable
-              onDragStart={(event) => onDrag(event)}
-              onDragEnd={(event) => onDrop(event, el)}
-              className="p-4  w-20  border bg-slate-500 text-gray-50"
-              key={el.id}
-            >
-              {el.element.content}
-            </div>
-          </DragbleElement>
-        ))}
-      </div> */}
-
+      <p className="text-gray-900 text-6xl  font-black">Welcome to AU BANK</p>
       <p className="text-gray-900 text-6xl font-black">
-        {" "}
-        Welcome to Narsingh Code
+        <span className="text-red-900 text-8xl font-black">
+          {" "}
+          open Dev Tools
+        </span>
       </p>
     </div>
   );
